1acd0743029c4ac52b94848eb1bffddceebbbc64
Zakiego <belummikir@gmail.com>
2022-03-28 17:15:16 +0800

    fix (app): increase sum history all account (#9)

--- apps/app/pages/api/app/submit.ts
+++ apps/app/pages/api/app/submit.ts
@@ -45,6 +45,9 @@ export default async function helloAPI(
 
   // add to historySummary
   const updateSummaryHistory = prisma.historySummary.updateMany({
+    where: {
+      userId: userId,
+    },
     data: {
       countTrain: {
         increment: 1,
