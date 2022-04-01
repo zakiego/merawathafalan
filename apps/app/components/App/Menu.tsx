05291db6de43a29b337b50eebdd39b07130505c6
Zakiego <belummikir@gmail.com>
2022-04-01 18:36:52 +0800

    feat (app): add menu

---
+++ apps/app/components/App/Menu.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+import LayoutMainPadding from "../Layout/LayoutMainPadding";
+import LayoutMaxWidth from "../Layout/LayoutMaxWidth";
+
+export default function Menu() {
+  const listMenu = [
+    {
+      name: "API",
+      link: "https://docs.merawathafalan.my.id/docs/intro",
+    },
+  ];
+
+  return (
+    <LayoutMaxWidth>
+      <LayoutMainPadding>
+        <div>
+          {listMenu.map((item) => {
+            return (
+              <div key={item.name}>
+                <Link href={item.link}>
+                  <a>
+                    <div className="btn-menu-animation rounded-lg py-3 px-4 font-semibold hover:cursor-pointer hover:bg-gray-200/70">
+                      {item.name}
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            );
+          })}
+          <div className="btn-menu-animation rounded-lg py-3 px-4 hover:bg-red-100/70">
+            <button
+              className="w-full text-left font-semibold"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </LayoutMainPadding>
+    </LayoutMaxWidth>
+  );
+}
--- apps/app/pages/app/profile.tsx
+++ apps/app/pages/app/profile.tsx
@@ -1,18 +1,25 @@
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 import AppBottomNavbar from "~/components/App/AppBottomNavbar";
 import AppProfile from "~/components/App/AppProfile";
 import AppTopNavbar from "~/components/App/AppTopNavbar";
+import Menu from "~/components/App/Menu";
 
 export default function Profile() {
   const { data } = useSession();
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   if (!data) return <div />;
 
   return (
     <div>
-      <AppTopNavbar />
-      <AppProfile name={data.user.name} image={data.user.image} />
+      <AppTopNavbar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
+      {isOpenMenu ? (
+        <Menu />
+      ) : (
+        <AppProfile name={data.user.name} image={data.user.image} />
+      )}
       <AppBottomNavbar />
     </div>
   );
